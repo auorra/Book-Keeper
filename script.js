3c91490d2ddc0bd3a513d1c60b8716f64dd46467
auorra <auora2020@protonmail.com>
2021-07-06 20:28:31 -0700

    commit on 'main'

--- script.js
+++ script.js
@@ -18,6 +18,22 @@ modalClose.addEventListener('click', () => modal.classList.remove('show-modal'))
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
 
 
+//validate form
+function validate(nameValue, urlValue) {
+	const expression = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+	const regex = new RegExp(expression);
+	if (!nameValue || !urlValue) {
+		alert('Please submit values for both fields.');
+		return false;
+	}
+	if (!urlValue.match(regex)) {
+		alert('Please provide a valid web address');
+		return false;
+	}
+	//Valid
+	return true;
+}
+
 //Handle Data from Form
 function storeBookmark(e){
 	e.preventDefault();
@@ -27,6 +43,9 @@ function storeBookmark(e){
 		urlValue = `https://${urlValue}`;
 	}
 	console.log(nameValue, urlValue);
+	if (!validate(nameValue, urlValue)) {
+		return false;
+	}
 }
 
 // Event Listener
