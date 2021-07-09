3207a61fa96dbbd1062798b71743edc0aa513e72
auorra <auora2020@protonmail.com>
2021-07-09 15:10:54 -0700

    build bookmarks

--- script.js
+++ script.js
@@ -37,6 +37,40 @@ function validate(nameValue, urlValue) {
 	
 }
 
+//Build bookmarks DOM
+function buildBookmarks() {
+	//build items
+	bookmarks.forEach((bookmark) => {
+		const { name, url } = bookmark;
+		//Item
+		const item = document.createElement('div');
+		item.classList.add('item');
+		//Close Icon
+		const closeIcon = document.createElement('i');
+		closeIcon.classList.add('fas', 'fa-times');
+		closeIcon.setAttribute('title', 'Delete Bookmark');
+		closeIcon.setAttribute('onclick', `deleteBookmark('${url}')`);
+		//Favicon / Link Container
+		const linkInfo = document.createElement('div');
+		linkInfo.classList.add('name');
+		//Favicon - not sure about favicon yet.
+		// const favicon = document.createElement('img');
+		// favicon.setAttribute('src');
+		// favicon.setAttribute('alt', 'Favicon');
+		
+		//Link
+		const link = document.createElement('a');
+		link.setAttribute('href', `${url}`);
+		link.setAttribute('target', '_blank');
+		link.textContent = name;
+		//Append to bookmarks container
+		linkInfo.append(link);
+		item.append(closeIcon, linkInfo);
+		bookmarksContainer.appendChild(item);
+		
+	});
+}
+
 //Fetch Bookmarks
 function fetchBookmarks() {
 	//Get bookmarks from localStorage if available
@@ -51,7 +85,7 @@ function fetchBookmarks() {
 		];
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	}
-	console.log(bookmarks);
+	buildBookmarks();
 }
 
 //Handle Data from Form
